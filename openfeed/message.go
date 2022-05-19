eb544d517c690837ef941f106eb66df660079238
Eero Pikat <eero.pikat@barchart.com>
2022-05-19 17:35:40 -0500

    Update message.go

--- openfeed/message.go
+++ openfeed/message.go
@@ -1,3 +1,9 @@
+// Copyright 2019 - 2022 Barchart.com, Inc. All rights reserved.
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file.
+
+// Package openfeed implements openfeed API.
+
 package openfeed
 
 type MessageType int32
